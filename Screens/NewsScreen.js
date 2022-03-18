084a2d124da98d2b11df88c4676339bd483b8807
Your Name <asmitasherlekar@gmail.com>
2022-03-18 09:37:51 +0530

    a

---
+++ Screens/NewsScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class NewsScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>NewsScreen</Text>
+            </View>
+        )
+    }
+}
